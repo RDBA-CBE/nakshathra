61fc3a22f615ead8abc67fb02e439148d0ed21e4
Pragathi-29 <pragathisarathi02@gmail.com>
2026-02-20 17:58:03 +0530

    Banner Image Updated

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,12 +1,17 @@
 'use client';
-
+'use client';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, EffectFade } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 
 export default function HeroSection() {
   return (
     <section className='relative w-full overflow-hidden'>
-      <div className='relative flex flex-col lg:flex-row w-full'>
+      <div className='relative flex flex-col-reverse lg:flex-row w-full'>
         {/* LEFT CONTENT */}
         <div className='relative z-10 w-full lg:w-1/2 bg-[#FBF6E6] flex items-center'>
           <div className='w-full px-6 sm:px-10 lg:px-16 xl:px-24 py-30 lg:py-30'>
@@ -57,12 +62,58 @@ export default function HeroSection() {
         </div>
 
         {/* RIGHT IMAGE */}
-        <div className='w-full lg:w-1/2 relative'>
-          <img
-            src='/home/banner-dark.webp'
-            alt='Luxury hotel bedroom'
-            className='absolute inset-0 w-full h-full object-cover object-center'
-          />
+        <div className='w-full lg:w-1/2 relative h-[750px] sm:h-[400px] lg:h-[700]'>
+          <Swiper
+            modules={[Autoplay, EffectFade]}
+            slidesPerView={1}
+            loop={true}
+            effect='fade'
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+            }}
+            className='w-full h-full'
+          >
+            <SwiperSlide>
+              <img
+                src='home/banner1.webp'
+                alt='Luxury hotel bedroom'
+                className='w-full object-cover object-center'
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                src='home/banner2.webp'
+                alt='Luxury hotel interior'
+                className='w-full object-cover object-center'
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                src='home/banner3.webp'
+                alt='Luxury hotel suite'
+                className='w-full object-cover object-center'
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                src='home/banner4.webp'
+                alt='Luxury hotel interior'
+                className='w-full object-cover object-center'
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img
+                src='home/banner-dark.webp'
+                alt='Luxury hotel interior'
+                className='w-full h-full object-cover object-center'
+              />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </section>
